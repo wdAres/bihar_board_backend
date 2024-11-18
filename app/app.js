760307d493a6d14faa59fff3e6b7fa5028e73ac0
Puneet Shrivastav <90194089+wdAres@users.noreply.github.com>
2024-11-18 14:34:25 +0530

    update

--- app/app.js
+++ app/app.js
@@ -6,7 +6,7 @@ const authRouter = require('./routes/authRoute')
 const contactRouter = require('./routes/contactRoute')
 const centerRouter = require('./routes/centerRoute')
 const studentRouter = require('./routes/studentRoute')
-const noticeRouter=require('./routes/noticeRoute')
+const noticeRouter = require('./routes/noticeRoute')
 const cors = require('cors')
 // TO PARSE JSON BODY
 app.use(express.json());
@@ -14,32 +14,20 @@ app.use(express.urlencoded({ extended: true }));
 // path join nahi kiya ? hum krte hai fir dikhate hai rukiye
 
 
-const allowedOrigins = [
-    'https://biharsanskritboard.netlify.app',
-    'https://biharsanskritboardcenter.netlify.app',
-    'https://biharsanskritboardadmin.netlify.app'
-];
-
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: '*', // Allows all origins 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type'],
-    credentials: true
+    credentials: true // Adjust if credentials are needed
 }));
 
 
 // app.use(cors())
-app.use('/api/v1/auth',authRouter)
-app.use('/api/v1/center',centerRouter)
-app.use('/api/v1/',contactRouter)
-app.use('/api/v1/',studentRouter)
-app.use('/api/v1/',noticeRouter)
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/center', centerRouter)
+app.use('/api/v1/', contactRouter)
+app.use('/api/v1/', studentRouter)
+app.use('/api/v1/', noticeRouter)
 
 // db checking
 sequelize.sync().then(() => { console.log('Database & tables created!'); });
